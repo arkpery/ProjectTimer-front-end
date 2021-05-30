3ede765f0e2e4d1cb2a3f3db97560a5e0bc524ff
arkpery <thomas.simoes.dev@gmail.com>
2021-05-30 19:46:45 +0200

    ProjectTimer-front-end

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbButtonModule} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbSidebarModule,
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule
+  ],
+  providers: [
+    NbSidebarService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
