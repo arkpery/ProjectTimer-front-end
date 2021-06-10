b85c64897968a8c4f1f968944f2379db9de8a1a7
arkpery <thomas.simoes.dev@gmail.com>
2021-06-10 05:51:01 +0200

    docker correction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GroupsComponent } from './groups/groups.component';
-import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { TasksComponent } from './tasks/tasks.component';
+
 
 const routes: Routes = [
   {path: 'projects', component: ProjectsComponent},
