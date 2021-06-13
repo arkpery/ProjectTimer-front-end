9f14233869f68f24a9957518ffc3aa156d2bc434
arkpery <thomas.simoes.dev@gmail.com>
2021-06-13 23:28:08 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { ProjectComponent } from './project/project.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UiButtonComponent } from './ui-button/ui-button.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { TimelineComponent } from './timeline/timeline.component';
@@ -40,7 +40,8 @@ import { ResponseResetPasswordComponent } from './pages/response-reset-password/
     HttpClientModule,
     FontAwesomeModule,
     NgSelectModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
   ],
